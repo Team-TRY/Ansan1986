a9e74a65a174e4ff77f22052b60cabb5cd0564b1
altair0307 <ds07017@naver.com>
2024-05-06 16:31:30 +0900

    [FEAT] 물체 충돌 감지
    특정물체를 태그를 이용하여 충돌 시 감지하는 기능

---
+++ Assets/Scripts/TestCubeScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Plane"))
+        {
+            Debug.Log("This is plane");
+        }
+    }
+}
