226b0f31852969ccb0eee8274850533ad159b09d
ekeia <1208238546@qq.com>
2024-05-28 20:42:57 +0900

    Revert "Dialogue System Demo version"
    This reverts commit 2029d5c284b0be4f695e7a18bd6e24774dc15f4d.

--- Assets/Dialogue_System/DialogueData.cs
+++
@@ -1,6 +0,0 @@
-[System.Serializable]
-public class DialogueData
-{
-    public string content;
-    public float displayTime;
-}
--- Assets/Dialogue_System/DialogueDataScriptableObject.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DialogueData", menuName = "ScriptableObjects/DialogueData", order = 1)]
-public class DialogueDataScriptableObject : ScriptableObject
-{
-    public List<DialogueData> dialogues;
-}
