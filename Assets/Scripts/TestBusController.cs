bfeea97b5f81f1098f7932947688f09fbdbcc363
Nwjwifh <111439484+Nwjwifh@users.noreply.github.com>
2024-05-29 02:48:49 +0900

    [Feat] 버스 정거장 시스템 및 경로 표시

--- Assets/Scripts/TestBusController.cs
+++ Assets/Scripts/TestBusController.cs
@@ -1,28 +1,38 @@
 using UnityEngine;
+using System.Collections;
 
 public class TestBusController : MonoBehaviour
 {
-    public float speed = 10.0f;         
-    public float rotationSpeed = 100.0f; 
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
     public float originalSpeed;
+    public Transform[] busStops; 
+    private int currentStopIndex = 0;
+    private bool isStopped = false;
+    public float stopDuration = 2f; 
 
     private Rigidbody rb;
+    public RouteVisualizer routeVisualizer; 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         originalSpeed = speed;
+        routeVisualizer.UpdateLineRenderer(currentStopIndex); 
     }
 
     void FixedUpdate()
     {
-        Move();
-        Turn();
+        if (!isStopped)
+        {
+            Move();
+            Turn();
+        }
     }
 
     void Move()
     {
-        float moveVertical = Input.GetAxis("Vertical"); 
+        float moveVertical = Input.GetAxis("Vertical");
         rb.MovePosition(rb.position + transform.forward * moveVertical * speed * Time.fixedDeltaTime);
     }
 
@@ -33,12 +43,39 @@ public class TestBusController : MonoBehaviour
         Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
         rb.MoveRotation(rb.rotation * turnRotation);
     }
-    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("BusStop") && other.transform == busStops[currentStopIndex])
+        {
+            StartCoroutine(StopAtBusStop());
+        }
+    }
+
+    IEnumerator StopAtBusStop()
+    {
+        isStopped = true;
+        SetSpeed(0);
+        yield return new WaitForSeconds(stopDuration);
+
+        if (currentStopIndex > 0)
+        {
+            routeVisualizer.UpdateLineRenderer(currentStopIndex);
+        }
+
+        busStops[currentStopIndex].gameObject.SetActive(false);
+
+        currentStopIndex = (currentStopIndex + 1) % busStops.Length;
+
+        ResetSpeed();
+        isStopped = false;
+    }
+
     public void SetSpeed(float newSpeed)
     {
         speed = newSpeed;
     }
-    
+
     public void ResetSpeed()
     {
         speed = originalSpeed;
