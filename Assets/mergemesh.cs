546149ae37998ae362d1a7d1919c52290b3b5bda
Bat-Undrakh <91944327+Bat-Undrakh@users.noreply.github.com>
2024-06-04 13:45:56 +0900

    map update

---
+++ Assets/mergemesh.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+// Copy meshes from children into the parent's Mesh.
+// CombineInstance stores the list of meshes.  These are combined
+// and assigned to the attached Mesh.
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class ExampleClass : MonoBehaviour
+{
+    void Start()
+    {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+
+            i++;
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.CombineMeshes(combine);
+        transform.GetComponent<MeshFilter>().sharedMesh = mesh;
+        transform.gameObject.SetActive(true);
+    }
+}
