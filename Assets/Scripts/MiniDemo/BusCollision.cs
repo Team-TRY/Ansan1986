820fad9ad9c2feb5ed7c0b1798da15badaa3e09b
한승우 <tmddn_00@naver.com>
2024-06-05 02:12:16 +0900

    [Feat] 빌딩 충돌시 타이머 빨간색으로 바뀜
    1초 동안만 바뀌는 것으로 기능 추가해야함.

--- Assets/Scripts/MiniDemo/BusCollision.cs
+++ Assets/Scripts/MiniDemo/BusCollision.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 public class BusCollision : MonoBehaviour
 {
     public GameManager gameManager;
+    public RemainTime remainTime;
+    
+    private void Start()
+    {
 
+    }
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.tag == "buliding")
         {
             Debug.Log("빌딩충돌");
+            remainTime.rTimeText.color = Color.red;
+            TimerColor(1.0f);
             RemainTime.rTime -= 5;
         }
 
@@ -23,6 +30,19 @@ public class BusCollision : MonoBehaviour
             RemainTime.rTime += 15;
             gameManager.NextLevel();
         }
+    }
+
+    public IEnumerator TimerColor(float duration)
+    {
+        float time = 0.0f;
+
+        while(time<1.0f)
+        {
+            time += Time.deltaTime/duration;
+            Debug.Log("코루틴");
+        }
+
+        yield return null;
     }
 }
 
