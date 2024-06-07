d5e10cb396a27124cb6a47e6b14c19883ea7c3ca
Nwjwifh <111439484+Nwjwifh@users.noreply.github.com>
2024-06-07 14:15:40 +0900

    [Feat] 마지막 정류장 트리거

---
+++ Assets/Scripts/BusCutsceneTrigger3.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.XR.Content.Interaction;
+
+public class BusCutsceneTrigger3 : MonoBehaviour
+{
+    public PlayableDirector timeLine;
+    public GameObject busObject;
+    public GameObject lever;
+    public GameObject animBus;
+    public GameObject busChar, busChar1, busChar2;
+    public GameObject animChar, animChar1, animChar2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            timeLine.Play();
+
+            XRLever buslever = lever.GetComponent<XRLever>();
+            buslever.state = LeverState.Neutral;
+            
+            busObject.SetActive(false);
+            animBus.SetActive(true);
+            GetComponent<BoxCollider>().enabled = false;
+            
+            StartCoroutine(ResumeGame());
+        }
+    }
+
+    IEnumerator ResumeGame()
+    {
+        yield return new WaitForSeconds(10);
+        
+        timeLine.Stop();
+        busObject.SetActive(true);
+        animBus.SetActive(false);
+        
+        busChar.SetActive(false);
+        busChar1.SetActive(false);
+        busChar2.SetActive(false);
+        animChar.SetActive(false);
+        animChar1.SetActive(false);
+        animChar2.SetActive(false);
+    }
+}
